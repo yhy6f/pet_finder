7b8c962a1d98a3633afdc81f5d566faf22838ff1
Jasmine Ye Han <jasmineyehan@gmail.com>
2022-02-10 20:23:21 -0500

    getting closer but still didn't get the token.js to work

--- token.js
+++ token.js
@@ -1,23 +1,26 @@
 function refreshToken() {
-let options = {
-    headers:{
-        grant_type:"client_credentials",
-        client_id:"5UAdwt5KiGDH1yxa5zYQJ4nR7mpS9NC4I4Slhf7LcYYCG0An9l",
-        client_secret:"ClM1upnfyamGeN9pQVSNyMIedgPXwbcQGaScdT0S"
+    let access_token = '';
+    const body = {
+        "grant_type": "client_credentials",
+        "client_id": "5UAdwt5KiGDH1yxa5zYQJ4nR7mpS9NC4I4Slhf7LcYYCG0An9l",
+        "client_secret": "ClM1upnfyamGeN9pQVSNyMIedgPXwbcQGaScdT0S"
     }
-}
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    })
+    .then(response => response.json())
+    .then(auth => {
+        // console.log(typeof(auth));
+        console.log(auth.access_token);
+        access_token = auth.access_token;
+    })
+    .catch(error => console.error(error))
 
-    fetch("https://api.petfinder.com/v2/oauth2/token",options).then(
-        response => response.json()
-    ).then(
-        data => data.xyz
-    )
+    return access_token
 }
-// the goal is to have refreshToken return a new access token every hour
-
-setTimeout(
-    () => {
-        const accessToken = refreshToken()
-    },
-    3600
-);
+    
+let accessToken = refreshToken();
